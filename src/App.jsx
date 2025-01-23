0a342f6586810c0d510f182ba188fc4f6d72f46e
mdubravic83 <mirsad.dubravic@megabooker.hr>
2025-01-23 20:47:37 +0100

    Update - states

--- src/App.jsx
+++ src/App.jsx
@@ -13,27 +13,66 @@ class App extends Component {
         { ime: "Ana", godine: 30 , grad: "Opatija" }
       ],
 
+
       poruka: "Dobrodošli u Algebru!"
 
     };
   }
 
+  // const brojacGodina [this.godine, setGodine]= this.state();
+
+  povecajGodine = (index) => {
+    const noviKorisnici = [...this.state.korisnici];
+    noviKorisnici[index].godine += 1; // Povećaj godine za korisnika na zadatom indeksu
+    this.setState({ korisnici: noviKorisnici });
+  };
+
+
   render() {
     const { korisnici, poruka  } = this.state;
 
 
+    // return (
+    //   <div>
+    //     {/* Prosljeđivanje props-a */}
+
+    //     <div>
+
+    //       <button onClick={() => setGodine(this.godine+1)}>Povećaj za 1 godinu</button>
+    //     </div>
+    //     <Component1 ime={korisnici[0].ime} godine={korisnici[0].godine} grad={korisnici[0].grad} />
+    //     <Component2 ime={korisnici[1].ime} godine={korisnici[1].godine} grad={korisnici[1].grad}>
+          
+    //        <Component2 />
+    //     <Component3 poruka={this.state.poruka} />
+    //     <MapComponent/>
+
+        
+
+
+        
+    //   </div>
+    // );
+
+
     return (
       <div>
-        {/* Prosljeđivanje props-a */}
-        <Component1 ime={korisnici[0].ime} godine={korisnici[0].godine} grad={korisnici[0].grad} />
-        <Component2 ime={korisnici[1].ime} godine={korisnici[1].godine} grad={korisnici[1].grad} />
-        <Component3 poruka={this.state.poruka} />
-        <MapComponent/>
-
-        
-
-
-        
+        <div>
+          <button onClick={() => this.povecajGodine(0)}>Povećaj Marku godine</button>
+          <button onClick={() => this.povecajGodine(1)}>Povećaj Ani godine</button>
+        </div>
+        <Component1
+          ime={korisnici[0].ime}
+          godine={korisnici[0].godine}
+          grad={korisnici[0].grad}
+        />
+        <Component2
+          ime={korisnici[1].ime}
+          godine={korisnici[1].godine}
+          grad={korisnici[1].grad}
+        />
+        <Component3 poruka={poruka} />
+        <MapComponent />
       </div>
     );
   }
